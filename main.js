a653dac749594e5b9dd3293db157b5c2f9fddde5
Jacoby04 <opdah023@morris.umn.edu>
2016-05-05 13:25:09 -0500

    Added in the initial files I need and drew the canvas for the game.

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by opdah023 on 5/5/16.
+ */
+
+'use strict';
+
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+};
