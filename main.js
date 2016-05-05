1e7a10c4aa00aca80f3fcfa836a2dddcd61711d0
Jacoby04 <opdah023@morris.umn.edu>
2016-05-05 13:42:44 -0500

    Added a game component in and a way to update the state of the game. Currently, the game compnent just moves right as it updates.

--- main.js
+++ main.js
@@ -4,16 +4,68 @@
 
 'use strict';
 
-function startGame() {
-    myGameArea.start();
-}
 
+// Game piece that will be given a constructor to make a component.
+var myGamePiece;
+
+
+/*
+Responsible for drawing the game area, starting the game, clearing the game area,
+setting up the update rate, and more.
+ */
 var myGameArea = {
     canvas : document.createElement("canvas"),
+
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 };
+
+
+/*
+Kicks off the game using the start function from the game area.
+Adds in the new game component.
+ */
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+
+/*
+Creates a game component object.
+ */
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        var ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+
+/*
+Is called based on the interval set in start for myGameArea.
+Performs the functions within with the intent on updating the state
+of the game.
+ */
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.x += 1; // Causes the red square to move right.
+    myGamePiece.update();
+}
+
+
+
